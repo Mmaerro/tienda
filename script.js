da8e8a339cd98258ac313712afc8feaba572456b
Mmaerro <89846526+Mmaerro@users.noreply.github.com>
2022-05-30 14:16:17 -0300

    sape

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var dropDown = document.getElementById('dropdown');
 var btnclose = document.getElementById('btnclose');
 var SideNav = document.getElementById("mySidenav");
+var overlay = document.getElementById("overlay");
 var header = document.getElementsByTagName('header');
 var nav = document.getElementById("nav");
 // cuando el usuario baja 20px del top del documento se aplica el cambio
@@ -24,9 +25,16 @@ function scrollFunction() {
 /* Set the width of the side navigation to 250px */
 function openNav() {
   SideNav.style.width = "250px";
-
+  overlay.style.width = "100vw";
+}
+function overlayClose(){
+ 
+  
 }
 /* Set the width of the side navigation to 0 */
-function closeNav() {
-  SideNav.style.width = "0";
+function closeNav() { 
+  overlay.addEventListener('click', function(){
+    SideNav.style.width = "0";
+    overlay.style.width = "0";
+  })
 }
